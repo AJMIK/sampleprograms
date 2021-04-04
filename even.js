643355c3a676911710c6d70f0529acd1eeb7eb21
AJMIK <ajmimkabeer24@gmail.com>
2021-04-04 12:21:04 +0530

    program compleated

---
+++ even.js
@@ -0,0 +1,7 @@
+var n=25
+if(n%2==0){
+    console.log("number is even")
+}
+else{
+    console.log("number is odd")
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+var a=56
+if (a%8==0) {
+    console.log("number is divisible by 8")
+} else {
+    console.log("number is not divisible by 8") 
+}
